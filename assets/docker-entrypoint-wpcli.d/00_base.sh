2d847f7fdf2cffdf71096b142fb5acbc93193ac2
James S. Moore <james@webtechhq.com>
2017-04-02 13:44:25 -0500

    Add is-installed detection

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -3,10 +3,13 @@ echo '....Configuring Base....'
 /usr/bin/env
 # install the core - variables starting with $WORDPRESS_ are
 # expected to be bash environment variables
-$WP core install --allow-root --debug \
-	--url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" \
-	--path="$WORDPRESS_PATH" \
-	--title="$WORDPRESS_TITLE" \
-	--admin_user="$WORDPRESS_ADMIN_USER" \
-	--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-	--admin_email="$WORDPRESS_ADMIN_EMAIL"
+if ! $(wp core is-installed); then
+		# perform a new install
+		$WP core install --allow-root --debug \
+			--url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" \
+			--path="$WORDPRESS_PATH" \
+			--title="$WORDPRESS_TITLE" \
+			--admin_user="$WORDPRESS_ADMIN_USER" \
+			--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+			--admin_email="$WORDPRESS_ADMIN_EMAIL"
+fi
