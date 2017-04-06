33a4bf49a488e6b72cd81a627760b096040c94f5
James S. Moore <james@webtechhq.com>
2017-04-05 20:00:01 -0500

    remove conditionals

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
+
+
+# download fresh copy
+echo '....Downloading Wordpress....'
+$WP core download
+
 echo '....Configuring Base....'
+# create a wp-config file
+$WP core config \
+  --dbname=$WORDPRESS_DB_NAME \
+  --dbuser=$WORDPRESS_DB_USER \
+  --dbpass=$WORDPRESS_DB_PASSWORD \
+  --locale=en_US
 
-# install the core - variables starting with $WORDPRESS_ are
-# expected to be bash environment variables
-if ! $($WP core is-installed); then
-
-    # download fresh copy
-    $WP core download
-
-    # create a wp-config file
-    $WP core config \
-      --dbname=$WORDPRESS_DB_NAME \
-      --dbuser=$WORDPRESS_DB_USER \
-      --dbpass=$WORDPRESS_DB_PASSWORD \
-      --locale=en_US
-
-		# perform a new install
-		$WP core install --allow-root --debug \
-			--url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" \
-			--path=$WORDPRESS_PATH \
-			--title="$WORDPRESS_TITLE" \
-			--admin_user="$WORDPRESS_ADMIN_USER" \
-			--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-			--admin_email="$WORDPRESS_ADMIN_EMAIL"
-fi
+# perform a new install
+$WP core install --allow-root --debug \
+	--url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" \
+	--path=$WORDPRESS_PATH \
+	--title="$WORDPRESS_TITLE" \
+	--admin_user="$WORDPRESS_ADMIN_USER" \
+	--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+	--admin_email="$WORDPRESS_ADMIN_EMAIL"
