35874c4d095aec04f2619a6c004a73e887d40db4
James S. Moore <james@webtechhq.com>
2017-04-05 23:43:08 -0500

    strip path from commands as it will now be configured via the yml config

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -2,11 +2,11 @@
 
 # download fresh copy
 echo '....Downloading Wordpress....'
-$WP core download --path=$WORDPRESS_PATH
+$WP core download
 
 echo '....Configuring Base....'
 # create a wp-config file
-$WP core config --path=$WORDPRESS_PATH
+$WP core config
 
 # perform a new install
 $WP core install --allow-root --debug --url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" --path=$WORDPRESS_PATH --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"
