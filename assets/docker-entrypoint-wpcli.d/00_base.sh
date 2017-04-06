f33f1d4da1b7893a50a9b01326cad440074b7841
James S. Moore <james@webtechhq.com>
2017-04-05 22:51:03 -0500

    remove control chars

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 # download fresh copy
 echo '....Downloading Wordpress....'
-$WP core download
+$WP core download --path=$WORDPRESS_PATH
 
 echo '....Configuring Base....'
 # create a wp-config file
-$WP core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --locale=en_US
+$WP core config --path=$WORDPRESS_PATH --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --locale=en_US
 
 # perform a new install
 $WP core install --allow-root --debug --url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" --path=$WORDPRESS_PATH --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"
