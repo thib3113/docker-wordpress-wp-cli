bed097e5dd0ee844ac469d5558647db730ed9dea
James S. Moore <james@webtechhq.com>
2017-04-05 20:18:55 -0500

    one-line CLI

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -7,17 +7,7 @@ $WP core download
 
 echo '....Configuring Base....'
 # create a wp-config file
-$WP core config \
-  --dbname=$WORDPRESS_DB_NAME \
-  --dbuser=$WORDPRESS_DB_USER \
-  --dbpass=$WORDPRESS_DB_PASSWORD \
-  --locale=en_US
+$WP core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --locale=en_US
 
 # perform a new install
-$WP core install --allow-root --debug \
-	--url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" \
-	--path=$WORDPRESS_PATH \
-	--title="$WORDPRESS_TITLE" \
-	--admin_user="$WORDPRESS_ADMIN_USER" \
-	--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-	--admin_email="$WORDPRESS_ADMIN_EMAIL"
+$WP core install --allow-root --debug --url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" --path=$WORDPRESS_PATH --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"
