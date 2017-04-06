e0b82b75432517ee65e7af626ca6450fec1de37a
James S. Moore <james@webtechhq.com>
2017-04-05 19:02:45 -0500

    removing env output for is-installed

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo '....Configuring Base....'
 
-if $($WP core is-installed); then
-    echo 'Wordpress is already installed..';
-    /usr/bin/env | grep 'W';
-fi
-
 # install the core - variables starting with $WORDPRESS_ are
 # expected to be bash environment variables
 if ! $($WP core is-installed); then
