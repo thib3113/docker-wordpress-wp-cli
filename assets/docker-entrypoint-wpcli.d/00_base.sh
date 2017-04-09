d1bf1cc04df353f762c526f4d2c9ac8cc077a0e1
James S. Moore <james@webtechhq.com>
2017-04-09 14:51:27 -0500

    wp-cli.yml, add --path to base

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -2,11 +2,11 @@
 
 # download fresh copy
 echo '....Downloading Wordpress....'
-$WP core download
+$WP core download --path=$WORDPRESS_PATH
 
 echo '....Configuring Base....'
 # create a wp-config file
-$WP core config
+$WP core config --path=$WORDPRESS_PATH --dbhost="$WORDPRESS_DB_HOST" --dbname="$WORDPRESS_DB_USER" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD"
 
 # perform a new install
-# $WP core install --allow-root --debug --url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" --path=$WORDPRESS_PATH --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"
+# $WP core install --allow-root --debug --path=$WORDPRESS_PATH --url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" --path=$WORDPRESS_PATH --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"
--- assets/init.sh
+++ assets/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Import WP-CLI config
+cp /assets/wp-cli.yml /var/www/html/wp-cli.yml
+chown www-data. /var/www/html/wp-cli.yml
+
 # Initialize WordPress
 /assets/docker-entrypoint-wpcli.d/init.sh
