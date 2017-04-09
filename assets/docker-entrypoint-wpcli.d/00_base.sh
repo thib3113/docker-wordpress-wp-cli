febeba7f18d07d82198a35b08d0a6f591cab92f5
James S. Moore <james@webtechhq.com>
2017-04-09 18:23:31 -0500

    WORDPRESS_TITLE str replace

--- assets/docker-entrypoint-wpcli.d/00_base.sh
+++ assets/docker-entrypoint-wpcli.d/00_base.sh
@@ -9,7 +9,7 @@ echo '....Configuring Base....'
 $WP core config --path=$WORDPRESS_PATH --dbhost="$WORDPRESS_DB_HOST" --dbname="$WORDPRESS_DB_USER" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD"
 
 # search replace whitespace with "-" in WORDPRESS_TITLE
-export WORDPRESS_TITLE="${WORDPRESS_TITLE/ /-}"
+export WORDPRESS_TITLE=`sed 's/ /-/g' <<< "$WORDPRESS_TITLE"`
 
 # perform a new install
 $WP core install --allow-root --debug --path=$WORDPRESS_PATH --url="$WORDPRESS_SITE_URL:$WORDPRESS_PORT" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"
