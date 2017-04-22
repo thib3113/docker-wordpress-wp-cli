6dab8d437249631c4ed0786d1625c62e38ac7261
James S. Moore <james@webtechhq.com>
2017-04-22 13:04:27 -0500

    Updates for Wordpress Traefik

--- assets/docker-entrypoint-wpcli.d/05_plugins.sh
+++ assets/docker-entrypoint-wpcli.d/05_plugins.sh
@@ -2,30 +2,29 @@
 echo 'Configuring Plugins'
 
 # remove defaults
-$WP plugin uninstall hello
-$WP plugin uninstall akismet
+$WP plugin uninstall hello akismet
 
 # install/activate project plugins
-$WP plugin install all-in-one-wp-security-and-firewall --activate
-$WP plugin install bwp-google-xml-sitemaps --activate
-$WP plugin install contact-form-7 --activate
+#$WP plugin install all-in-one-wp-security-and-firewall --activate
+#$WP plugin install bwp-google-xml-sitemaps --activate
+#$WP plugin install contact-form-7 --activate
 #$WP plugin install postman-smtp --activate
 #$WP plugin install listo --activate
-$WP plugin install custom-login
-$WP plugin install disable-password-reset --activate
-$WP plugin install peters-login-redirect --activate
-
-$WP plugin install ml-slider --activate
-$WP plugin install megamenu --activate
-
-$WP plugin install wp-google-fonts --activate
-
-$WP plugin install googleanalytics --activate
-$WP plugin install google-analytics-dashboard-for-wp --activate
-
-# Contexture Page Security
-
-$WP plugin install contexture-page-security --activate
-$WP option update ad_opt_login_anon true
+# $WP plugin install custom-login
+# $WP plugin install disable-password-reset --activate
+# $WP plugin install peters-login-redirect --activate
+#
+# $WP plugin install ml-slider --activate
+# $WP plugin install megamenu --activate
+#
+# $WP plugin install wp-google-fonts --activate
+#
+# $WP plugin install googleanalytics --activate
+# $WP plugin install google-analytics-dashboard-for-wp --activate
+#
+# # Contexture Page Security
+#
+# $WP plugin install contexture-page-security --activate
+# $WP option update ad_opt_login_anon true
 
 $WP plugin status
