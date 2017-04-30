c11ec091e7c85453534e5ce77186210dd86324db
James S. Moore <james@webtechhq.com>
2017-04-30 17:28:44 -0500

    Adding Unzip and disabling other inits for now

--- assets/docker-entrypoint-wpcli.d/init.sh
+++ assets/docker-entrypoint-wpcli.d/init.sh
@@ -4,9 +4,9 @@ MWD='/assets/docker-entrypoint-wpcli.d'
 $MWD/00_base.sh
 $MWD/05_plugins.sh
 $MWD/10_theme.sh
-$MWD/15_media.sh
-$MWD/20_content.sh
-$MWD/25_menu.sh
+#$MWD/15_media.sh
+#$MWD/20_content.sh
+#$MWD/25_menu.sh
 #$MWD/50_widgets.sh
 #$MWD/80_users.sh
 
