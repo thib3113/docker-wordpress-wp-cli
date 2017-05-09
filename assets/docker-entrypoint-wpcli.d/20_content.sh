086849e39de814301b9b2c105b72a607b70225d0
James S. Moore <james@webtechhq.com>
2017-05-09 17:51:11 -0500

    Fix Category creation

--- assets/docker-entrypoint-wpcli.d/20_content.sh
+++ assets/docker-entrypoint-wpcli.d/20_content.sh
@@ -10,8 +10,8 @@ $WP post delete 2 --force # Sample page
 ##############################################################
 ################ create post categories ######################
 ##############################################################
-$WP term create category Announcements --slug='announcements' --description='Announcements / Breaking News'
-$WP term create category Events --slug='events' --description='Events Calendar'
+$WP term create category Announcements --slug='announcements' --description='\"Announcements / Breaking News\"'
+$WP term create category Events --slug='events' --description='\"Events Calendar\"'
 $WP term create category Newsletter --slug='newsletter' --description='Newsletter'
 
 $WP import /assets/wxr/pages.xml --authors=skip
