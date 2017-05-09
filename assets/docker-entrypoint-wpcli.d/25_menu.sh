1bdcb067ddc6888ed3b6febdadadeb73efa8f3ef
James S. Moore <james@webtechhq.com>
2017-05-09 16:36:26 -0500

    Removing more spaces in titles

--- assets/docker-entrypoint-wpcli.d/25_menu.sh
+++ assets/docker-entrypoint-wpcli.d/25_menu.sh
@@ -20,7 +20,7 @@ news_id=$($WP menu item add-custom main-menu "News" "$BASEURL/news" --porcelain)
 $WP menu item add-custom main-menu "Calendar-of-Events" "$BASEURL/category/events" --parent-id=$news_id
 $WP menu item add-custom main-menu "Announcements" "$BASEURL/category/announcements" --parent-id=$news_id
 $WP menu item add-custom main-menu "Newsletters" "$BASEURL/category/newsletter" --parent-id=$news_id
-$WP menu item add-custom main-menu "WCA News" "$BASEURL/category/wca" --parent-id=$news_id
+$WP menu item add-custom main-menu "WCA-News" "$BASEURL/category/wca" --parent-id=$news_id
 $WP menu item add-custom main-menu "NMRA" "$BASEURL/category/nmra" --parent-id=$news_id
 $WP menu item add-custom main-menu "NMGA" "$BASEURL/category/nmga" --parent-id=$news_id
 
@@ -28,8 +28,8 @@ $WP menu item add-custom main-menu "Legislative" "$BASEURL/legislative"
 
 member_id=$($WP menu item add-custom main-menu "Members" "$BASEURL/member" --porcelain)
 $WP menu item add-custom main-menu "Find-an-HCP" "$BASEURL/find-an-hcp" --parent-id=$member_id
-$WP menu item add-custom main-menu "Find an Agent" "$BASEURL/member/find-an-agent" --parent-id=$member_id
-members_home_id=$($WP menu item add-custom main-menu "Members Home" "$BASEURL/members/member-home" --parent-id=$member_id --porcelain)
+$WP menu item add-custom main-menu "Find-an-Agent" "$BASEURL/member/find-an-agent" --parent-id=$member_id
+members_home_id=$($WP menu item add-custom main-menu "Members-Home" "$BASEURL/members/member-home" --parent-id=$member_id --porcelain)
 $WP menu item add-custom main-menu "Account-Receivable-Report" "$BASEURL/member/member-home/account-receivable-report" --parent-id=$members_home_id
 $WP menu item add-custom main-menu "Premium-Computation" "$BASEURL/member/member-home/premium-computation" --parent-id=$members_home_id
 $WP menu item add-custom main-menu "Loss-Run-Report" "$BASEURL/member/member-home/loss-run-report" --parent-id=$members_home_id
@@ -39,7 +39,7 @@ $WP menu item add-custom main-menu "Loss-Ratio-Report" "$BASEURL/member/member-h
 $WP menu item add-custom main-menu "Resources" "$BASEURL/member/member-home/resources" --parent-id=$members_home_id
 
 agent_id=$($WP menu item add-custom main-menu "Agent" "$BASEURL/agent" --porcelain)
-$WP menu item add-custom main-menu "Become a FISIF Agent" "$BASEURL/agent/become-a-fisif-agent" --parent-id=$agent_id
+$WP menu item add-custom main-menu "Become-An-Agent" "$BASEURL/agent/become-a-fisif-agent" --parent-id=$agent_id
 agent_home_id=$($WP menu item add-custom main-menu "Agent Home" "$BASEURL/agent/agent-home" --parent-id=$agent_id --porcelain)
 $WP menu item add-custom main-menu "Submit-Quote-Application" "$BASEURL/agent/submit-quote-application" --parent-id=$agent_id
 $WP menu item add-custom main-menu "Application-Forms" "$BASEURL/agent/application-forms" --parent-id=$agent_id
