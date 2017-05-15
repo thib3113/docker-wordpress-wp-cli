ca1b3c4f3727aca6f42021759c346706e2eddbcc
James S. Moore <james@webtechhq.com>
2017-05-14 22:25:24 -0500

    enable most plugins

--- assets/docker-entrypoint-wpcli.d/05_plugins.sh
+++ assets/docker-entrypoint-wpcli.d/05_plugins.sh
@@ -4,23 +4,24 @@ echo 'Configuring Plugins'
 # remove defaults
 $WP plugin uninstall hello akismet
 
-# install wp importer
-$WP plugin install wordpress-importer --activate
-
 # force SSL
 cd $WORDPRESS_PATH/wp-content/plugins
 wget -v https://gist.githubusercontent.com/jamessmoore/6d9e58d83bb52909e64e040f3077b695/raw/9a701fec3fc9090917ac53c26cf1ad60d4747ec1/force-ssl-url-scheme.php
 $WP plugin activate force-ssl-url-scheme
 
+# install wp importer and defaults
+$WP plugin install wordpress-importer --activate
+
 # install/activate project plugins
+$WP plugin install capability-manager-enhanced --activate
 $WP plugin install all-in-one-wp-security-and-firewall --activate
 $WP plugin install bwp-google-xml-sitemaps --activate
-#$WP plugin install contact-form-7 --activate
-#$WP plugin install postman-smtp --activate
+$WP plugin install contact-form-7 --activate
+$WP plugin install postman-smtp --activate
 $WP plugin install listo --activate
 $WP plugin install custom-login --activate
 $WP plugin install disable-password-reset --activate
-$WP plugin install peters-login-redirect --activate
+$WP plugin install simple-pdf-exporter --activate
 #
 $WP plugin install ml-slider --activate
 $WP plugin install megamenu --activate
@@ -28,12 +29,13 @@ $WP plugin install megamenu --activate
 # wp-google-fonts causes errors with the theme
 # $WP plugin install wp-google-fonts --activate
 #
-# $WP plugin install googleanalytics --activate
-# $WP plugin install google-analytics-dashboard-for-wp --activate
+$WP plugin install googleanalytics --activate
+$WP plugin install google-analytics-dashboard-for-wp --activate
 #
 # # Contexture Page Security
 #
 $WP plugin install contexture-page-security --activate
 $WP option update ad_opt_login_anon true
+$WP plugin install peters-login-redirect --activate
 
 $WP plugin status
