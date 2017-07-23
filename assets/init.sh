63972db1b946e7084f39444e85aaca9fd4a17ffe
James Moore <james.moore@ipsoft.com>
2017-07-23 11:55:15 -0500

    more work to disable SSL redirect

--- assets/init.sh
+++ assets/init.sh
@@ -4,9 +4,9 @@
 cp /assets/wp-cli.yml /var/www/html/wp-cli.yml
 chown www-data. /var/www/html/wp-cli.yml
 
+# Initialize WordPress
+# /assets/docker-entrypoint-wpcli.d/init.sh
+
 # Initialize the Webserver
 echo 'Starting web services....';
 apache2-foreground;
-
-# Initialize WordPress
-/assets/docker-entrypoint-wpcli.d/init.sh
